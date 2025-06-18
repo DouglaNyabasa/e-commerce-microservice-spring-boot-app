563b41f591bcff8ad3d895dfbceda64c80632307
douglanyabasa <douglasnyabasa400@gmail.com>
2025-06-18 14:10:46 +0200

    notification service first push

---
+++ services/notifications/src/test/java/com/ecommerce/pm/notifications/NotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.pm.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
